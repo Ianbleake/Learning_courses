279e4c3e2e953e41d5524b1f8e2f87be46859894
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-05-20 11:53:40 -0600

    Class objets

--- JS/Js/Objetos.js
+++ JS/Js/Objetos.js
@@ -3,6 +3,22 @@ window.addEventListener('load',()=>{
   boton.addEventListener('click',()=>{
     boton.style.border = "10px solid green";
   });
-
-  
+  //Crar un objeto
+  const player = {
+    name: 'Ian',
+    life: '99',
+    power: '10',
+    talk: ()=>{ return "Hola"; }
+  };
+  //Acceder a las propiedades del objeto
+  console.log(player);  
+  console.log(player.name);
+  console.log(player.life);
+  console.log(player.power); 
+  //Añadir una propiedad a un objeto
+  player.weapon = "sword";
+  console.log(player.weapon);
+  console.log(player);
+  //Metodo de un objeto
+  console.log(player.talk());
 });
