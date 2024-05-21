41242a8f512a59541403ec0a87b7766db63b3161
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-05-21 11:41:28 -0600

    JSon class

--- JS/Js/Objetos.js
+++ JS/Js/Objetos.js
@@ -1,24 +1,34 @@
+//Objetos
 window.addEventListener('load',()=>{
   var boton = document.querySelector('#press');
   boton.addEventListener('click',()=>{
     boton.style.border = "10px solid green";
   });
-  //Crar un objeto
-  const player = {
-    name: 'Ian',
-    life: '99',
-    power: '10',
-    talk: ()=>{ return "Hola"; }
-  };
-  //Acceder a las propiedades del objeto
-  console.log(player);  
-  console.log(player.name);
-  console.log(player.life);
-  console.log(player.power); 
-  //Añadir una propiedad a un objeto
-  player.weapon = "sword";
-  console.log(player.weapon);
-  console.log(player);
-  //Metodo de un objeto
-  console.log(player.talk());
 });
+
+const attack = ()=>{console.log("use fireball");};
+
+//Crar un objeto
+const player = {
+  name: 'Ian',
+  life: '99',
+  power: '10',
+  talk: ()=>{ return "Hola"; },
+  fireball: attack
+};
+
+//Acceder a las propiedades del objeto
+console.log(player);  
+console.log(player.name);
+console.log(player.life);
+console.log(player.power); 
+//Añadir una propiedad a un objeto 
+player.weapon = "sword";
+console.log(player.weapon);
+console.log(player);
+
+//Metodos de un objeto
+console.log(player.talk());
+player.fireball();
+
+//JSON
