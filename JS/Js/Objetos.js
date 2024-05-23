df76a203e72fef55b6907f8465cd06ff4cad6d3c
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-05-23 12:03:36 -0600

    Reestructuracion de Objetos

--- JS/Js/Objetos.js
+++ JS/Js/Objetos.js
@@ -45,3 +45,46 @@ const usuario = {
 }
 
 console.log(JSON.stringify(usuario));
+
+//Desestructuracion de objetos
+const cita = {
+  paciente: "Ivan",
+  lugar: "Hospital Angeles",
+  consultorio: 2,
+  motivo: "Tos",
+  historial: ["tos","covid","gastritis"]
+}
+
+let {paciente,lugar: Hospital,consultorio,motivo,expefiente = 1999} = cita;
+
+console.log(paciente);
+console.log(Hospital);
+console.log(consultorio);
+console.log(motivo);
+console.log(expefiente);
+
+
+//Tambien puede hacerse con JSON
+fetch("../Js/informacion.Json").then(response => {return response.json();}).then(user => {
+  let {name,life} = user;
+  console.log(name);
+  console.log(life);
+});
+
+//Reestructurado de Objetos y clonado de objetos
+
+const expediente = {
+  ...structuredClone(cita),
+  paciente: "Ivan Rangel",
+  tratamiento: "naproxeno",
+  doctor: "Alejandra Sanchez"
+}
+
+console.log("Nuevo: ", expediente.historial[0] );
+console.log("Viejo: ", cita.historial[0] );
+
+expediente.historial[0] = "Dolor de estomago";
+
+console.log("Nuevo: ", expediente.historial[0] );
+console.log("Viejo: ", cita.historial[0] );
+
